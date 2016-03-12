9650fa45f42780c478927e7ec6b5ba274b66bdbe
jiayi <yjiahustcs@gmail.com>
2016-03-13 00:44:56 +0800

    First Commit

---
+++ scripts/toggle.js
@@ -0,0 +1,12 @@
+var navClass = document.getElementById('navbar-collapse').classList;
+document.getElementById('collapse').onclick = function()
+{
+	if(navClass.contains('collapse'))
+	{
+		navClass.remove('collapse');
+	}
+	else
+	{
+		navClass.add('collapse');
+	}
+};
